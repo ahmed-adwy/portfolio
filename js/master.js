0e1d4526e74371af1335a9833eb72680c1e6f6bf
ahmedadwy <ahmedadwy200@gmail.com>
2023-04-09 23:16:25 +0200

    Update code

--- js/master.js
+++ js/master.js
@@ -1,32 +1,17 @@
-// Smooth scrolling for anchor links
-const navLinks = document.querySelectorAll('nav a');
+// Smooth scrolling effect for navigation links
+const navLinks = document.querySelectorAll('nav ul li a');
 
-navLinks.forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const targetId = link.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
-    const targetPosition = targetElement.offsetTop;
-    window.scrollTo({
-      top: targetPosition,
-      behavior: 'smooth'
-    });
-  });
-});
+for (const link of navLinks) {
+  link.addEventListener('click', clickHandler);
+}
 
-// Form submission handling
-const form = document.querySelector('form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const messageInput = document.querySelector('#message');
-
-form.addEventListener('submit', e => {
+function clickHandler(e) {
   e.preventDefault();
-  const formData = {
-    name: nameInput.value,
-    email: emailInput.value,
-    message: messageInput.value
-  };
-  console.log(formData); // Replace with your own form submission logic
-  form.reset();
-});
+  const href = this.getAttribute('href');
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: 'smooth'
+  });
+}
